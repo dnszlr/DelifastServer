4e98cb938f97335c57ba797a3b637122b036896d
dennis <Dennis.Zhukki@gmail.com>
2021-06-01 14:48:36 +0200

    Initial commit

---
+++ routes/delifastRouter.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+let controller = require('../controllers/delifastController');
+
+router.get('/', controller.getLandingPage);
+router.post('/checkout', controller.receivePayment);
+router.get('/send', controller.sendPayment);
+router.post("/refund", controller.refundPayment);
+router.get('/createuser', controller.createUser);
+router.get('/clienttoken', controller.getClientToken);
+
+module.exports = router;
